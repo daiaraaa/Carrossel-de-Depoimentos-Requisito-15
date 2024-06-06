a9bacb026e707e1aea0b5c664b20a6423f9de97c
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-06-05 22:00:53 -0300

    Update script.js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+let count = 1
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){}, 2000)
+
+function nextImagem(){
+  count++;
+  if (count>4) {
+    count = 1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+}
